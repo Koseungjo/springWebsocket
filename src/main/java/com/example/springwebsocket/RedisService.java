941437aed36dc1b0e71449626b9b918732a83d7e
imokoreacorp <imokoreacorp@gmail.com>
2023-11-07 11:48:55 +0900

    history update

--- src/main/java/com/example/springwebsocket/RedisService.java
+++ src/main/java/com/example/springwebsocket/RedisService.java
@@ -1,11 +1,13 @@
 package com.example.springwebsocket;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.DataType;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -20,9 +22,22 @@ public class RedisService {
     }
 
     public List<String> getMessagesFromList(String listKey, long start, long end) {
-        ListOperations<String, String> listOps = redisTemplate.opsForList();
-        return listOps.range(listKey, start, end);
+        DataType dataType = redisTemplate.type(listKey);
+
+        if (dataType == DataType.LIST) {
+            // 데이터 타입이 리스트라면 그대로 반환
+            ListOperations<String, String> listOps = redisTemplate.opsForList();
+            return listOps.range(listKey, start, end);
+        } else if (dataType == DataType.STRING) {
+            // 데이터 타입이 문자열이라면 리스트에 포장하여 반환
+            String singleMessage = redisTemplate.opsForValue().get(listKey).toString();
+            return Collections.singletonList(singleMessage);
+        } else {
+            // 다른 데이터 타입은 빈 리스트나 적절한 처리를 할 수 있습니다.
+            return Collections.emptyList();
+        }
     }
+
     public String getValues(String key){
         //opsForValue : Strings를 쉽게 Serialize / Deserialize 해주는 Interface
         ValueOperations<String, String> values = redisTemplate.opsForValue();
